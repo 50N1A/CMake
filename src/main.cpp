6294584cb367efee8d08767b35a58ad86c21f3b1
Sonia <126466631+50N1A@users.noreply.github.com>
2023-03-06 10:14:55 +0300

    Added main.cpp and CMAkeLists.txt

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World"<<std::endl;
+	int a;
+	std::cin >>a;
+	std::cout << std::endl << a;
+	std::cin.get();
+	std::cin.get();
+	return 0;
+}
